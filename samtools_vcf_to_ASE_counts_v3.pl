f9c8d3007f2033c4de52140830a7e0ffb124313f
Cheyenne Payne <cypayne@stanford.edu>
2020-06-28 16:13:50 -0700

    fixed ASE counting issues, compartibility with new R script

--- samtools_vcf_to_ASE_counts_v3.pl
+++ samtools_vcf_to_ASE_counts_v3.pl
@@ -1,14 +1,14 @@
 #perl! -w
+# perl $path/samtools_vcf_to_ASE_counts_v3.pl $overlap1
+# input files are: .par1/.par2
 
 my $infile=shift(@ARGV); chomp $infile;
 open IN, $infile or die "cannot open infile\n";
 
-my $gtf=shift(@ARGV); chomp $gtf;
-
 open OUT, ">"."$infile"."_ASE_counts";
 
 while(my $line=<IN>){
-    if($line !~ '#'){
+  if($line !~ '#'){
     chomp $line;
 #    print "$line\n";
     my @fields=split(/\t/,$line);
@@ -19,21 +19,27 @@ while(my $line=<IN>){
     my $alldepths=$dp4[0]; chomp $alldepths;
     $alldepths=~ s/DP4=//g;
     my @indiv_depths=split(/,/,$alldepths);
+    # in DP4 field, index 0 and 1 are ref allele counts
+    #               index 2 and 3 are alt allele counts
     my $a1=$indiv_depths[0]+$indiv_depths[1];
     my $a2=$indiv_depths[2]+$indiv_depths[3];
     
+    # ref base in pos 2, alt base in pos 3
     my $base1=$fields[2]; chomp $base1;
     my $base2=$fields[3]; chomp $base2;
+    my $ref=$fields[7]; chomp $ref;
     my $alt=$fields[8]; chomp $alt;
 #    print "$base1\t$base2\t$alt\t$fields[0]\t$a1\t$a2\n";
    
-    if(($alt eq '.') or ($alt eq $base1) or ($alt eq $base2)){
-	if(length($gtf)>0){
-	    print OUT $fields[4],"\t",$fields[1],"\t",$fields[2],"\t",$fields[3],"\t$a1\t$a2\n";
-	}else{
-	    print OUT $fields[0],"\t",$fields[1],"\t",$fields[2],"\t",$fields[3],"\t$a1\t$a2\n";
-	}#gtf style or not
-    }#check the agreement with the ref/alt base
+    # check that the bases are as expected from AIMs file,
+    # organize according to ref/alt
+    if((($alt eq '.') or ($alt eq $base2)) and ($ref eq $base1)){
+	    print OUT $fields[0],"\t",$fields[1],"\t",$base1,"\t",$base2,"\t$a1\t$a2\n";
+    }
+    elsif((($alt eq '.') or ($alt eq $base1)) and ($ref eq $base2)){
+      # counts switched to reflect flipped ref/alt alleles
+	    print OUT $fields[0],"\t",$fields[1],"\t",$base1,"\t",$base2,"\t$a2\t$a1\n";
+	  }
 
-    }#not header line
+  }#not header line
 }#for all lines
